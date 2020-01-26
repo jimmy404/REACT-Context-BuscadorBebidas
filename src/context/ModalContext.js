e6c70145938afac76d36bb51432ab3a971b87dc7
Jimmy <bruno_pier@hotmail.com>
2020-01-26 18:49:52 -0300

    Adding ModalProvider import&code - App.js

---
+++ src/context/ModalContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+// Crear context
+
+export const ModalContext = createContext();
+
+
+const ModalProvider = () => {
+
+    //state provider
+    const [ idreceta, guardarIdReceta ] = useState(null);
+
+
+    return (
+        <ModalContext.Provider
+            value={{
+
+            }}
+        >
+            {props.children}
+        </ModalContext.Provider>>
+    );
+}
+
+export default ModalProvider;
+
