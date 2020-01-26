d5a36cf31c9ba07dc08234b0b6dd9e8873b96f8b
Jimmy <bruno_pier@hotmail.com>
2020-01-26 13:59:13 -0300

    Adding container folder - components -

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+            <header className="bg-alert">
+                Buscador de Bebidas
+            </header>
+    );
+}
+
+export default Header;
