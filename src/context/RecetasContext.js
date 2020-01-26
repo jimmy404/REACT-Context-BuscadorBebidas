77bdccee1afef006f4a4cad60e691cc0919f5b16
Jimmy <bruno_pier@hotmail.com>
2020-01-26 17:55:54 -0300

    Adding guardarRecetas code in useEffect - RecetasContext.js

--- src/context/RecetasContext.js
+++ src/context/RecetasContext.js
@@ -18,8 +18,12 @@ const RecetasProvider = (props) => {
         if(consulta){
             const obtenerRecetas = async () =>{
                 const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${nombre}&c=${categoria}`;
-                console.log(url);
+
+                const resultado = await axios.get(url);
+                //console.log(resultado.data.drinks);
+                guardarRecetas(resultado.data.drinks);
             }
+
             obtenerRecetas();
         }
 
