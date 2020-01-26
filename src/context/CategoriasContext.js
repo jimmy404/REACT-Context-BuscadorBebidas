0af4419d23a2e545bac34c275bff74ea5bcde4b8
Jimmy <bruno_pier@hotmail.com>
2020-01-26 14:33:47 -0300

    Adding context folder & CategoriasContext.js file

---
+++ src/context/CategoriasContext.js
@@ -0,0 +1,38 @@
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+// Creando el context
+
+export const CategoriasContext = createContext();
+
+// Provider = func&state
+
+const CategoriasProvider = (props) => {
+
+    //State context
+    const [categorias, guardarCategorias] = useState([]);
+
+    //ejecutar el llamado de la api
+    useEffect(() => {
+        const obtenerCategorias = async () => {
+            const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+
+            const categorias = await axios.get(url);
+            guardarCategorias(categorias.data.drinks);
+        }
+        obtenerCategorias();
+    }, []);
+
+    return(
+        <CategoriasContext.Provider
+            value={{
+                categorias
+            }}
+        >
+            {props.children}
+        </CategoriasContext.Provider>
+    )
+
+}
+
+export default CategoriasProvider;
