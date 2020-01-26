7e828fff8766630415bac417e632a3d8efd980de
Jimmy <bruno_pier@hotmail.com>
2020-01-26 18:59:13 -0300

    Adding onClick event in button - Receta.js

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {ModalContext} from '../context/ModalContext';
 
 const Receta = ({receta}) => {
+
+    //extraer valores del context
+    const {guardarIdReceta} = useContext(ModalContext);
+
+
     return (
         <div className="col-md-4 md-3">
             <div className="card">
@@ -14,6 +20,9 @@ const Receta = ({receta}) => {
                     <button
                         type="button"
                         className="btn btn-block btn-primary"
+                        onClick={() => {
+                            guardarIdReceta(receta.idDrink)
+                        }}
                     >Ver Receta</button>
                 </div>
             </div>
