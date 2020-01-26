34441c5eb58af4572b84f0a5339962146ef5c330
Jimmy <bruno_pier@hotmail.com>
2020-01-26 19:08:57 -0300

    Adding url&const&code - get receta data - ModalContext.js

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -10,6 +10,20 @@ const ModalProvider = (props) => {
 
     //state provider
     const [ idreceta, guardarIdReceta ] = useState(null);
+    const [receta, guardarReceta ] = useState({});
+
+    // cuando tengo la receta, llamado a al api
+
+    useEffect(() => {
+        const obtenerReceta = async () => {
+            if(!idreceta) return;
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}
+            `;
+            const resultado = await axios.get(url);
+            guardarReceta(resultado.data.drinks[0]);
+        }
+        obtenerReceta();
+    }, [idreceta]);
 
 
     return (
