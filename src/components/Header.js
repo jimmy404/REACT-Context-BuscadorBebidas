92866bb9e2d75c6935d410206177da457cfdcafb
Jimmy <bruno_pier@hotmail.com>
2020-01-27 12:52:54 -0300

    Adding styles - Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-            <header className="bg-alert">
+            <header className="bg-alert display-4">
                 Buscador de Bebidas
             </header>
     );
